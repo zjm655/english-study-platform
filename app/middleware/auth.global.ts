5b969746bb4b8b434b4c55e778d52f7b8be2f09e
zjm <18075904248@163.com>
2026-07-13 10:19:01 +0800

    优化鉴权逻辑

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -1,7 +1,9 @@
 import { useToVerify } from '~/composables/user'
+import { useUserStore } from '~/store/useUserStore'
 
 export default defineNuxtRouteMiddleware((to) => {
-    if (import.meta.server) return
+    if (import.meta.server || useUserStore().isVerify) return
     const verify = useToVerify()
     verify.userToVerify()
+    useUserStore().isVerify = true
 })
--- app/store/useUserStore.ts
+++ app/store/useUserStore.ts
@@ -2,10 +2,10 @@
 export const useUserStore = defineStore('user', () => {
   const user = ref<LoginResPayload | null>(null)
   const isLogin = ref<boolean>(false)
-
+  const isVerify = ref<Boolean>(false)
   function setUser(data: LoginResPayload) {
     user.value = data
   }
 
-  return { user, isLogin, setUser }
+  return { user, isLogin, isVerify, setUser }
 })
